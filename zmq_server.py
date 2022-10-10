08486542ef56c5441bbaf5131cc41812132b5c29
SMATousi <mohamadali.tousi@gmail.com>
2022-10-09 21:29:50 -0500

    The warming up mode added.

--- zmq_server.py
+++ zmq_server.py
@@ -67,31 +67,31 @@ def capture_3D(site, case_path, case_number, server_no, master_ID, sync_par, syn
             if master_ID == nano_ID:
 
 
-                command = "k4arecorder --external-sync master -e 20000 -r 5 -l 1 -d WFOV_UNBINNED -c 3072p " + case_path + "/" + "Animal_" + str(case_number) + "_nano_" + server_no + ".mkv"
+                command = "k4arecorder --external-sync master -e 20000 -r 5 -l 1 -d WFOV_UNBINNED -c 1080p " + case_path + "/" + "Animal_" + str(case_number) + "_nano_" + server_no + ".mkv"
                 os.system("export DISPLAY=:0")
                 time.sleep(2)
             else:
-                command = "k4arecorder --external-sync sub --sync-delay " + str((int(nano_ID[1])-1) * 160) + "-e 20000 -r 5 -l 1 -d WFOV_UNBINNED -c 3072p " + case_path + "/" + "Animal_" + str(case_number) + "_nano_" + server_no + ".mkv"
+                command = "k4arecorder --external-sync sub --sync-delay " + str((int(nano_ID[1])) * 160) + " -e 20000 -r 5 -l 1 -d WFOV_UNBINNED -c 1080p " + case_path + "/" + "Animal_" + str(case_number) + "_nano_" + server_no + ".mkv"
                 os.system("export DISPLAY=:0")
         else:
             if master_ID == nano_ID:
 
 
-                command = "k4arecorder --external-sync master -e 20000 -r 5 -l 1 -d WFOV_UNBINNED -c 3072p " + case_path + "/" + "Animal_" + str(case_number) + "_nano_" + server_no + ".mkv"
+                command = "k4arecorder --external-sync master -e 20000 -r 5 -l 1 -d WFOV_UNBINNED -c 1080p " + case_path + "/" + "Animal_" + str(case_number) + "_nano_" + server_no + ".mkv"
                 os.system("export DISPLAY=:0")
                 time.sleep(2)
             else:
-                command = "k4arecorder --external-sync sub -e 20000 -r 5 -l 1 -d WFOV_UNBINNED -c 3072p " + case_path + "/" + "Animal_" + str(case_number) + "_nano_" + server_no + ".mkv"
+                command = "k4arecorder --external-sync sub -e 20000 -r 5 -l 1 -d WFOV_UNBINNED -c 1080p " + case_path + "/" + "Animal_" + str(case_number) + "_nano_" + server_no + ".mkv"
                 os.system("export DISPLAY=:0")
     else:
-        command = "k4arecorder -r 5 -l 1 -d WFOV_UNBINNED -c 3072p " + case_path + "/" + "Animal_" + str(case_number) + "_nano_" + server_no + ".mkv"
+        command = "k4arecorder -r 5 -l 1 -d WFOV_UNBINNED -c 1080p " + case_path + "/" + "Animal_" + str(case_number) + "_nano_" + server_no + ".mkv"
         os.system("export DISPLAY=:0")
         time.sleep(2)
 
 
     
     process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE)
-    time.sleep(2)
+    time.sleep(10)
     os.system("rsync -av {source} vigir3d@192.168.0.21:/home/vigir3d/Datasets/cattle_scans/".format(source = "~/Images/"+ site + "/" + "Animal_" + str(case_number) + "_nano_" + server_no + ".mkv"))
 
     process_pass = process.wait()
@@ -160,6 +160,18 @@ while True:
         case_number = message[2]
         site_path = os.path.join(base_path, site)
         os.system("rsync -av {source} vigir3d@192.168.0.21:/home/vigir3d/Datasets/cattle_scans/".format(source = site_path + "/" + "Animal_" + str(case_number) + "_nano_" + nano_ID + '.txt'))
+        socket.send_string("Done")
+        message = socket.recv()
+        message = message.decode("utf-8")
+
+    elif message1[0] == "w":
+        os.system("/home/vigir/Documents/Azure-Kinect-Sensor-SDK/build/bin/viewer_opengl &")
+        socket.send_string("Done")
+        message = socket.recv()
+        message = message.decode("utf-8")
+
+    elif message1[0] == "wk":
+        os.system("killall viewer_opengl")
         socket.send_string("Done")
         message = socket.recv()
         message = message.decode("utf-8")
